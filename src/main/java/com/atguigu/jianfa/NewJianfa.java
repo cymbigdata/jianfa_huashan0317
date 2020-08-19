f26d77bc5aa7590a667d4506de297661f08befe3
cymbigdata <cym_bigdata@126.com>
2020-08-19 20:44:42 +0800

    新剑法

---
+++ src/main/java/com/atguigu/jianfa/NewJianfa.java
@@ -0,0 +1,10 @@
+package com.atguigu.jianfa;
+
+public class NewJianfa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
